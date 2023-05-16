e1f9bc18631910bffffb664a3963e1458a9bd7a2
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-16 14:24:13 -0400

    git commit -m(add append)

--- lib/linkedlist_bb.rb
+++ lib/linkedlist_bb.rb
@@ -2,4 +2,26 @@ class LinkedList
     attr_reader :head
     def initialize
         @head = nil
+        @string_arr = []
+    end
+
+    def last_node(start)
+        head = start
+        if start.next_node == nil
+            return start
+        else
+            last_node(start.next_node)
+        end
+    end
+        
+    def append(string)
+        
+        if  @head == nil
+            @head = Node.new(string)
+        else
+            @head = @head
+            last_node(head).add_next_node(Node.new(string))
+        end
+        @string_arr << string
+        string
     end
