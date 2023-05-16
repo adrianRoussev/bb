a136cb39391623552f1d4baeffde4060d3fcc8af
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-16 16:06:03 -0400

    git commit -m(add node tests)

---
+++ spec/node_bb_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/node_bb'
+
+RSpec.describe Node do
+it 'exists' do
+    node = Node.new("plop")
+    expect(node).to be_an_instance_of(Node)
+end
+
+it "has data" do
+    node = Node.new("plop")
+    expect(node.data).to eq("plop")
+end
+
+it "can have other data" do
+    node = Node.new("dop")
+    expect(node.data).to eq("dop")
+end
+
+it "has next node nil by default " do
+node = Node.new("plop")
+    expect(node.next_node).to eq(nil)
+end
+
+it "can have another next node"do
+node2= Node.new("dop")
+node = Node.new("plop", node2)
+    expect(node.next_node).to eq(node2)
+end
+end
