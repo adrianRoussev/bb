9eed01a368c8340f95df7a13875f0e4687fa4d00
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-05-16 19:28:35 -0400

    edit beatbox class

--- lib/bb.rb
+++ lib/bb.rb
@@ -1,24 +1,24 @@
 require './linkedlist_bb'
 class BeatBox
     attr_reader :list
-  
+
     def initialize
-      @list = LinkedList.new
+        @list = LinkedList.new
     end
-  
+
     def append(sounds)
-      strings = sounds.split(' ')
-      strings.each do |string|
+        strings = sounds.split(' ')
+        strings.each do |string|
         list.append(string)
-      end
-      sounds
+        end
+    sounds
     end
-  
-    def beats_count
-      list.node_count
-    end
-  
-    def play
-      `say -r 500 -v Boing #{list.to_string}`
-    end
-  end
+
+def beats_count
+    list.node_count
+end
+
+def play
+    `say -r 500 -v Boing #{list.to_string}`
+end
+end
